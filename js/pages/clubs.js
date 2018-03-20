78a639461de5ed52fb4d8620386cfb392183c857
Zieheavy <ryanvandenbroek@gmail.com>
2018-03-20 15:13:31 +0100

    .

--- js/pages/clubs.js
+++ js/pages/clubs.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  var images = [];
+  var selectedImage = 0;
+
   $("#js-toggle-collapsed").click(function() {
     var elem = $("#js-toggle-collapsed").text();
     if (elem == "Read More") {
@@ -13,7 +16,23 @@ $(document).ready(function(){
   });
 
   $('.js-image').on('click',function(){
-    console.log("click")
-    $('#myModel').css('display','block')
+    images = [];
+    selectedImage = 0;
+    for(var i = 0; i < $(this).siblings().length+1; i++){
+      var temp = $('.js-image')[i].currentSrc.split('/')
+      var tempString = "img/" + temp[temp.length-1]
+      images.push({src: tempString})
+      if($(this).attr('src') == tempString){
+        selectedImage = i;
+      }
+    }
+    $('#myModal').css('display','block')
+    $("#img01").attr("src",$(this).attr('src'))
+    // $('#caption').text($(this).attr('alt'))
   })
+
+  $('.close').on('click',function(){
+    $('#myModal').css('display','none')
+  })
+
 });
